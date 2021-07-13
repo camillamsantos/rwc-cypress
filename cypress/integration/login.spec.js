e9e187a2bc20e547324ef9a869e971a0dcb79197
camillamsantos <84816792+camillamsantos@users.noreply.github.com>
2021-07-12 22:46:24 -0300

    Update login.spec.js

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -3,6 +3,19 @@ import login from '../support/pages/login'
 
 context('Login', () => {
     it('Realizar login com sucesso', () => {
+        //     //acessar a pagina de login
+        //     cy.visit('login')
+
+        //     //preencher o formulario
+
+        //     cy.get('input[ng-model*=email]').type(Cypress.config().user.email)
+
+        //    // cy.pause()
+
+        //     cy.get('input[ng-model*=password]').type(12345678)
+        //     //submeter o formulario de login
+        //     cy.get('button.btn-primary').click()
+
         login.acessarLogin()
         login.preencherFormulario()
         login.submeterFormulario()
@@ -11,3 +24,7 @@ context('Login', () => {
 
     });
 });
+
+////input[ng-model*=email]
+//input[ng-model*=password]
+//button.btn-primary
